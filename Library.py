ec64cb29c16763dffd1901b6cd1bdb671b759c4b
Kensuke KOBAYASHI <165744970+pbuy4zo8@users.noreply.github.com>
2024-08-31 10:16:54 +0900

    Create Library.py
    first commit

---
+++ Library.py
@@ -0,0 +1,28 @@
+'''
+  written by K.Kobayashi(jeffy890)
+  2024.08.31
+
+  Library Schema
+  Object oriented ver
+'''
+
+
+class Library:
+  def __init__(self):
+    pass
+
+class Person:
+  def __init__(self):
+    pass
+
+class Object:
+  def __init__(self):
+    pass
+
+class Area:
+  def __init__(self):
+    pass
+
+class Concept:
+  def __init__(self):
+    pass
